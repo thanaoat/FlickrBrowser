873111247306587d74ddf9a0ca39cf20dc6ab6cd
Thananon Kumkrong <thananon.oat@gmail.com>
2019-11-03 14:55:56 +0700

    Can view photo.

--- app/src/main/java/academy/learnprogramming/flickrbrowser/PhotoDetailsActivity.kt
+++ app/src/main/java/academy/learnprogramming/flickrbrowser/PhotoDetailsActivity.kt
@@ -1,22 +1,25 @@
 package academy.learnprogramming.flickrbrowser
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.content_photo_details.*
 
-import kotlinx.android.synthetic.main.activity_photo_details.*
-
-class PhotoDetailsActivity : AppCompatActivity() {
+class PhotoDetailsActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photo_details)
-        setSupportActionBar(toolbar)
+        activateToolbar(true)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+        val photo = intent.getSerializableExtra(PHOTO_TRANSFER) as Photo
+
+        photo_title.text = photo.title
+        photo_tags.text = photo.tags
+        photo_author.text = photo.author
+        Picasso.with(this).load(photo.link)
+            .error(R.drawable.placeholder)
+            .placeholder(R.drawable.placeholder)
+            .into(photo_image)
     }
 
 }
--- app/src/main/java/academy/learnprogramming/flickrbrowser/RecyclerItemClickListener.kt
+++ app/src/main/java/academy/learnprogramming/flickrbrowser/RecyclerItemClickListener.kt
@@ -2,8 +2,10 @@ package academy.learnprogramming.flickrbrowser
 
 import android.content.Context
 import android.util.Log
+import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.View
+import androidx.core.view.GestureDetectorCompat
 import androidx.recyclerview.widget.RecyclerView
 
 class RecyclerItemClickListener(context: Context, recyclerView: RecyclerView, private val listener: OnRecyclerClickListener)
@@ -15,8 +17,35 @@ class RecyclerItemClickListener(context: Context, recyclerView: RecyclerView, pr
         fun onItemLongClick(view: View, position: Int)
     }
 
+    // add the gestureDetector
+    private val gestureDetector = GestureDetectorCompat(context, object: GestureDetector.SimpleOnGestureListener() {
+
+        override fun onSingleTapUp(e: MotionEvent): Boolean {
+            Log.d(TAG, "onSingleTapUp: starts")
+            val childView = recyclerView.findChildViewUnder(e.x, e.y)
+            if (childView != null) {  // Added 2018-12-18
+                Log.d(TAG, ".onSingleTapUp calling listener.onItemClick")
+                listener.onItemClick(childView, recyclerView.getChildAdapterPosition(childView))
+                return true
+            } else {
+                return false
+            }
+        }
+
+        override fun onLongPress(e: MotionEvent) {
+            Log.d(TAG, "onLongPress: starts")
+            val childView = recyclerView.findChildViewUnder(e.x, e.y)
+            if (childView != null) {  // Added 2018-12-18
+                Log.d(TAG, ".onInterceptTouchEvent calling listener.onItemLongClick")
+                listener.onItemLongClick(childView, recyclerView.getChildAdapterPosition(childView))
+            }
+        }
+    })
+
     override fun onInterceptTouchEvent(rv: RecyclerView, e: MotionEvent): Boolean {
         Log.d(TAG, ".onInterceptTouchEvent: stars $e")
-        return super.onInterceptTouchEvent(rv, e)
+        val result = gestureDetector.onTouchEvent(e)
+//        return super.onInterceptTouchEvent(rv, e)
+        return result
     }
 }
